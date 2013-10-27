00c42c7796b9a1ca0ff5be6f52aab3505359ded7
adt1983 <adt1983@4a2e60ad-6e70-489f-915e-2f1c45f13921>
2013-10-27 18:57:08 +0000

    Disallow holes during polygon simplification.

--- Server/trunk/src/com/hazardalert/LosslessPolygonSimplifier.java
+++ Server/trunk/src/com/hazardalert/LosslessPolygonSimplifier.java
@@ -2,6 +2,8 @@ package com.hazardalert;
 
 import com.hazardalert.common.Assert;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Polygon;
 
 /*
@@ -12,9 +14,11 @@ public class LosslessPolygonSimplifier {
 	public static Polygon simplify(Polygon input) {
 		Polygon simple = (Polygon) com.vividsolutions.jts.simplify.TopologyPreservingSimplifier.simplify(input, 0.01);
 		simple = (Polygon) simple.union(input); // re-attach any polygons we 'oversimplified'
+		simple = new Polygon((LinearRing) simple.getExteriorRing(), null, new GeometryFactory()); // drop any holes
 		Polygon convexHull = (Polygon) input.convexHull();
 		Geometry diff = input.difference(simple);
 		new Assert(input.getArea() <= simple.getArea() && simple.getArea() <= convexHull.getArea());
+		new Assert(0 == simple.getNumInteriorRing());
 		//new Assert(convexHull.covers(input));
 		//new Assert(convexHull.covers(simple));
 		new Assert(diff.isEmpty() || (diff.getArea() / simple.getArea()) < 0.0001); // shouldn't .covers be enough?
